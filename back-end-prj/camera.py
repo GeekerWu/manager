5318fbed5b65d92ce5b724ebc9775f627a2cb1a9
GeekerWu <finally-weapon@hotmail.com>
2020-12-02 20:31:18 +0800

    update

--- back-end-prj/camera.py
+++ back-end-prj/camera.py
@@ -1,11 +1,13 @@
-import cv2 as cv
-cap=cv.VideoCapture(0)
+import os
+os.environ['LD_PRELOAD'] = '/usr/lib/arm-linux-gnueabihf/libatomic.so.1'
+import cv2
+cap=cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()                        #读取帧
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) #灰度化展示
-    cv.imshow('frame',gray)
-    if cv.waitKey(1) & 0xFF == ord('q'):          #按‘q’退出
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #灰度化展示
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):          #按‘q’退出
         break
 #释放资源并关闭窗口
 cap.release()
-cv.destroyAllWindows()
+cv2.destroyAllWindows()
